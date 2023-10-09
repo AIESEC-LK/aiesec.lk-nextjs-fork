b1270022e97444cffd4889c125eb5e6a41186a4d
Ashan <ashandimanthalk@gmail.com>
2023-10-10 02:03:27 +0530

    Revert "update fonts"
    This reverts commit 47de8940fe8c90d57cfba9b6cf92bde06d563802.

--- components/CounterSection.js
+++ components/CounterSection.js
@@ -17,34 +17,34 @@ export default function Counter(){
                     <div className=" grid sm:grid-cols-4 sm:text-lg text-sm text-white m-14 grid-cols-1 grid-rows-4">
                         <div className=" flex justify-center align-middle sm:mb-0 mb-4">
                             <ul className=" list-none">
-                                <li className=" flex justify-center sm:text-7xl text-4xl font-bold"><span><VisibilitySensor><CountUpComponent end={1500}/></VisibilitySensor></span><span>+</span></li>
+                                <li className=" flex justify-center sm:text-7xl text-4xl"><span><VisibilitySensor><CountUpComponent end={1500}/></VisibilitySensor></span><span>+</span></li>
                                 <li className=" flex justify-center">Active Members</li>
                             </ul>
                         </div>
                         <div className=" flex justify-center align-middle sm:mb-0 mb-4">
                             <ul className=" list-none">
-                                <li className=" flex justify-center sm:text-7xl text-4xl font-bold"><span id="unicount">19</span><span>+</span></li>
+                                <li className=" flex justify-center sm:text-7xl text-4xl"><span id="unicount">19</span><span>+</span></li>
                                 <li className=" flex justify-center">Universities</li>
                             </ul>
                         </div>
                         <div className=" flex justify-center align-middle sm:mb-0 mb-4">
                             <ul className=" list-none">
-                                <li className=" flex justify-center sm:text-7xl text-4xl font-bold"><span id="prjcount">100</span><span>+</span></li>
+                                <li className=" flex justify-center sm:text-7xl text-4xl"><span id="prjcount">100</span><span>+</span></li>
                                 <li className=" flex justify-center">Social Projects Every Year</li>
                             </ul>
                         </div>
                         <div className=" flex justify-center align-middle sm:mb-0 -mb-1">
                             <ul className=" list-none">
-                                <li className=" flex justify-center sm:text-7xl text-4xl font-bold"><span id="expcount">1500</span><span>+</span></li>
+                                <li className=" flex justify-center sm:text-7xl text-4xl"><span id="expcount">1500</span><span>+</span></li>
                                 <li className=" flex justify-center">Experiences Every Year</li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className=" flex justify-center"><span className=" text-7xl text-aiesec-mid-grey text-center font-semibold">AIESEC CREATES YOUNG LEADERS FROM</span></div>
+            <div className=" flex justify-center"><span className=" text-7xl text-aiesec-mid-grey text-center">AIESEC CREATES YOUNG LEADERS FROM</span></div>
             <div className=" relative flex overflow-x-hidden">
-                <div className=" animate-marquee whitespace-nowrap font-bold"><span className=" text-8xl text-aiesec-mid-grey"><span className=" text-aiesec-red">VOLUNTEERING EXPERIENCES</span> & <span className=" text-aiesec-orange">PROFESSIONAL INTERNSHIPS</span> & <span className=" text-aiesec-red">TEACHING OPPORTUNITIES</span> & <span className=" text-aiesec-orange">MEMBERSHIP EXPERIENCES</span>.</span></div>
+                <div className=" animate-marquee whitespace-nowrap"><span className=" text-8xl text-aiesec-mid-grey"><span className=" text-aiesec-red">VOLUNTEERING EXPERIENCES</span> & <span className=" text-aiesec-orange">PROFESSIONAL INTERNSHIPS</span> & <span className=" text-aiesec-red">TEACHING OPPORTUNITIES</span> & <span className=" text-aiesec-orange">MEMBERSHIP EXPERIENCES</span>.</span></div>
             </div>
         </section>
     )
