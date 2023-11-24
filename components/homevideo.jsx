9dabc80835474fc36d91d4e7a72a0018be0a2966
Ashan <ashandimanthalk@gmail.com>
2023-11-24 15:30:23 +0530

    updated pp

--- components/homevideo.jsx
+++ components/homevideo.jsx
@@ -1,17 +1,19 @@
 import MultiText from "@components/Micro_components/Multitext";
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Video from "next-video";
+
 
 function HomeVideo() {
+
   return (
     <section id="home" className="relative w-screen overflow-hidden">
-      <div className="w-screen z-0 sm:mt-0 mt-20">
-        {/* Provide the correct path to your video file */}
-        <video src="/aslvideosm.mp4" muted autoPlay loop />
+      <div className=" w-screen z-0 sm:mt-0 mt-20">
+        <video src={require("@public/aslvideosm.mp4")} muted autoPlay loop/>
       </div>
-      <div className="absolute bottom-5 left-5 sm:bottom-10 sm:left-10 md:bottom-18 md:left-18 xl:bottom-72 xl:left-32 z-20">
+      <div className="absolute bottom-5 left-5 sm:bottom-10 sm:left-10 md:bottom-18 md:left-18 xl:bottom-72 xl:left-32 z-20 ">
         <MultiText />
       </div>
-      <div className="absolute bg-gradient-to-t from-aiesec-blue to-transparent h-2/3 z-10 w-screen bottom-0"></div>
+      <div className=" absolute bg-gradient-to-t from-aiesec-blue to-transparent h-2/3 z-10 w-screen bottom-0"></div>
     </section>
   );
 }
