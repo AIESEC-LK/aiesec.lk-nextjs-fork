37bd3eaba2dfd35e1cb3672b52886f81454a29f6
Ashan <ashandimanthalk@gmail.com>
2023-12-25 11:30:26 +0530

    routing

---
+++ pages/products/global-volunteer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Volunteer() {
+  return (
+    <h2>Welcome to Global Volunteer</h2>
+  )
+}
+
+export default Volunteer
--- pages/products/global-volunteer/volunteer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function volunteer() {
-  return (
-    <div>Global Volunteer</div>
-  )
-}
-
-export default volunteer
