72ae89719ea0f8f0de9097efc6d7d9f2209293e2
adithyakalhan15 <adithyakalhan15@gmail.com>
2023-12-26 22:46:00 +0530

    Added our projects to igv

--- pages/products/global-volunteer/index.jsx
+++ pages/products/global-volunteer/index.jsx
@@ -5,13 +5,14 @@ import HomeVideo from "@components/homevideo";
 import Counter from "@components/CounterSection";
 import VideoTestimonials from '@components/products/VideoTestimonials';
 import Layout from '@components/products/Layout';
+import Projectsigv from '@components/products/projects';
 
 function Volunteer() {
   return (
     <>
       <HtmlHead title={"Global Volunteer - AIESEC in Sri Lanka"} description={"Global Volunteer is a cross-cultural experience for youth (age 18 – 30) who want to gain personal development and leave an impact on the world."}/>
-      <Layout textColor="white" bgColor="global-volunteer">
-        <Nav/>
+      <Layout textColor="" bgColor="global-volunteer">
+      <Nav/>
       {/* <HomeVideo/>
       <Counter/> */}
       <br />
@@ -20,6 +21,9 @@ function Volunteer() {
       <br />
       <h1>Description</h1>
       <VideoTestimonials/>
+      <Projectsigv/>
+      <br/>
+      <br/>
       <h1>This is Page 1</h1>
     </Layout>
 
