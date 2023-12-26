6dab85b2875b150b39f824d8c65465a29a0e9cb6
Ashan <ashandimanthalk@gmail.com>
2023-12-27 01:35:17 +0530

    update colours

--- pages/products/global-volunteer/index.jsx
+++ pages/products/global-volunteer/index.jsx
@@ -11,11 +11,10 @@ function Volunteer() {
   return (
     <>
       <HtmlHead title={"Global Volunteer - AIESEC in Sri Lanka"} description={"Global Volunteer is a cross-cultural experience for youth (age 18 – 30) who want to gain personal development and leave an impact on the world."}/>
-
-      <Layout textColor="white" bgColor="global-volunteer">
+      <Nav/>
+      <Layout textColor="global-volunteer">
       {/* Use bgColor everywhere you need global volunteer colour */}
-        <Nav/>
-      <Layout textColor="" bgColor="global-volunteer">
+      
       {/* <HomeVideo/>
       <Counter/> */}
       <br />
@@ -24,10 +23,9 @@ function Volunteer() {
       <br />
       <h1>Description</h1>
       <VideoTestimonials/>
-      <Projectsigv/>
-      <br/>
-      <br/>
       <h1>This is Page 1</h1>
+      <Projectsigv/>
+
     </Layout>
 
 
