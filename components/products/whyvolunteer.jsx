86858a54647dd9dc306f197c7d406b9967c3f96e
adithyakalhan15 <adithyakalhan15@gmail.com>
2023-12-30 01:38:41 +0530

    igv_responsive update

--- components/products/whyvolunteer.jsx
+++ components/products/whyvolunteer.jsx
@@ -6,18 +6,20 @@ function Whyvolunteer(){
     const pointdata = Data[3].whyvolunteer[1].points;
 
     return(
-        <div className=" flex flex-wrap bg-white">
-            <div className=" w-2/5 flex-wrap -translate-x-20">
+        <div className=" flex sm:flex-wrap bg-white flex-col sm:flex-row">
+            <div className=" sm:w-2/5 flex-wrap sm:-translate-x-20 -translate-x-8">
                 <Image src={Data[3].whyvolunteer[0].leftimage[0].path} alt="leftimage" width="800" height="800"/>
             </div>
-            <div className=" w-3/5 py-6">
-                <h2 className=" text-4xl text-global-volunteer font-bold pb-6">Why Volunteer with AIESEC</h2>
-                <div>
+            <div className=" sm:w-3/5 py-6">
+                <h2 className=" text-4xl text-global-volunteer font-bold pb-6 sm:text-left text-center">Why Volunteer with AIESEC</h2>
+                <div className=" mx-5">
                     {
                         pointdata && pointdata.map((data)=>(
                                 <div key={data.id} className=" flex my-5">
-                                    <Image src={Data[3].whyvolunteer[0].leftimage[0].tick} alt="greentick" width={30} height={20}/>
-                                    <h3 className=" text-xl ml-4 font-medium">{data.content}</h3>
+                                    <div className="w-1/12 min-w-30 lg:max-w-20 md:max-w-15 sm:justify-end mx-3">
+                                        <Image src={Data[3].whyvolunteer[0].leftimage[0].tick} alt="greentick" width={30} height={30}/>
+                                    </div>
+                                    <h3 className="w-11/12 text-xl font-medium ">{data.content}</h3>
                                 </div>
                             ))
                     }
