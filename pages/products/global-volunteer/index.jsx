04096abe3a22e95ad1470b9ad17786258835cc87
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-02-05 10:34:59 +0530

    fix: components

--- pages/products/global-volunteer/index.jsx
+++ pages/products/global-volunteer/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import {usePathname} from "next/navigation";
+import {useEffect, useState} from "react";
+
 import HtmlHead from "@components/partner-portal/HtmlHead";
 import Nav from "@components/Nav";
 import HomeVideo from "@components/homevideo";
 import Counter from "@components/CounterSection";
 import VideoTestimonials from "@components/products/VideoTestimonials";
-import Layout from "@components/products/Layout";
 import Projects from "@components/products/projects";
 import ProjectDestinations from "@components/products/projects2";
 import Whyvolunteer from "@components/products/whyvolunteer";
@@ -14,10 +16,6 @@ import Footer from "@components/Footer";
 import ContactFrom from "@components/form";
 import Intro from "@components/products/Intro";
 import SplashScreen from "@components/SplashScreen";
-
-
-import {usePathname} from "next/navigation";
-import {useEffect, useState} from "react";
 import Process from "@components/products/Process";
 
 function Volunteer() {
@@ -30,43 +28,6 @@ function Volunteer() {
     if (isLoading) return;
   }, [isLoading]);
 
-  //Product Intro Data
-  const introData = GVData.map((data) => (
-    data.basicdetails && data.basicdetails.map((pdata) => (
-      <Intro
-        key={pdata.id}
-        title={pdata.title}
-        color={pdata.color}
-        logo={pdata.logo}
-        description={pdata.content}
-        duration={pdata.duration}
-        organization={pdata.organization}
-      />
-    ))
-  ));
-
-  //ProjectData
-  const projectDetails = GVData.map((info) => (
-    info.ourprojects && info.ourprojects.map((project) => (
-      <Projects
-        key={project.id}
-        title={project.title}
-        img={project.path}
-      />
-    ))
-  ));
-
-  //DestinationData
-  const destinationDetails = GVData.map((info) => (
-    info.ourdestination && info.ourdestination.map((destination) => (
-      <ProjectDestinations
-        key={destination.id}
-        countryname={destination.countryname}
-        projecttype={destination.projecttype}
-        path={destination.path}
-      />
-    ))
-  ));
 
   return (
     <>
@@ -83,16 +44,55 @@ function Volunteer() {
         <Nav />
       <HomeVideo />
       <Counter />
-      {introData}
+      {/* Product Intro */}
+      <div>
+        {
+          GVData.map((data) => (
+            data.basicdetails && data.basicdetails.map((pdata) => (
+              <Intro
+                key={pdata.id}
+                title={pdata.title}
+                color={pdata.color}
+                logo={pdata.logo}
+                description={pdata.content}
+                duration={pdata.duration}
+                organization={pdata.organization}
+              />
+            ))
+          ))}
+      </div>
       <VideoTestimonials />
+      {/* Our Projects */}
       <h2 className="text-global-volunteer flex justify-center text-3xl font-bold pb-14 p-8">Our Projects</h2>
       <div className="flex flex-wrap justify-center">
-      {projectDetails}
+        {
+          GVData.map((info) => (
+            info.ourprojects && info.ourprojects.map((project) => (
+              <Projects
+                key={project.id}
+                title={project.title}
+                img={project.path}
+              />
+            ))
+          ))}
+        
       </div>
+
       <h2 className="text-global-volunteer flex justify-center text-3xl font-bold pb-14 p-8">Our Destinations</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {destinationDetails}
+        {
+          GVData.map((info) => (
+            info.ourdestination && info.ourdestination.map((destination) => (
+              <ProjectDestinations
+                key={destination.id}
+                countryname={destination.countryname}
+                projecttype={destination.projecttype}
+                path={destination.path}
+              />
+            ))
+          ))}
       </div>
+      <br />
       <Whyvolunteer />
       <Process />
       <Signupbanner />
