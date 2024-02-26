ac79071139c1503242744ef5a1f3656b794d6372
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-02-27 03:09:30 +0530

    add: global-volunteer

--- pages/products/global-volunteer/index.jsx
+++ pages/products/global-volunteer/index.jsx
@@ -1,7 +1,7 @@
 // Global Volunteer
 import React from "react";
-import {usePathname} from "next/navigation";
-import {useEffect, useState} from "react";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 import GVData from "@pages/products/global-volunteer/data.json";
 
 import HtmlHead from "@components/partner-portal/HtmlHead";
@@ -22,7 +22,7 @@ import FnQ from "@components/FnQ";
 import WhyProducts from "@components/products/WhyProducts";
 
 function Volunteer() {
-  const volunteerColor = 'global-volunteer';
+  const volunteerColor = "global-volunteer";
   const pathname = usePathname();
   //change the path here
   const isHome = pathname === "/products/global-volunteer";
@@ -32,143 +32,145 @@ function Volunteer() {
     if (isLoading) return;
   }, [isLoading]);
 
-
   return (
     <>
       <HtmlHead
-        title={"Global Volunteer - AIESEC in Sri Lanka"}
+        title={"Global Talent - AIESEC in Sri Lanka"}
         description={
           "Global Volunteer is a cross-cultural experience for youth (age 18 – 30) who want to gain personal development and leave an impact on the world."
         }
       />
       {isLoading && isHome ? (
-      <SplashScreen finishLoading={() => setIsLoading(false)}/>
-    ) : (
-      <div className=" overflow-hidden">
-        <Nav />
-      <HomeVideo />
-      <Counter />
-      {/* Product Intro */}
-      <div>
-        {
-          GVData.map((data) => (
-            data.basicdetails && data.basicdetails.map((pdata) => (
-              <Intro
-                key={pdata.id}
-                title={pdata.title}
-                color={pdata.color}
-                logo={pdata.logo}
-                description={pdata.content}
-                duration={pdata.duration}
-                organization={pdata.organization}
-                url={pdata.url}
-                
-                
-              />
-            ))
-          ))}
-      </div>
-      {/* Why Products */}
-      <div className="flex flex-col justify-around items-center md:flex-row pb-10">
-        {
-          GVData.map((data) => (
-            data.whyproducts && data.whyproducts.map((pdata) => (
-              <WhyProducts
-                key={pdata.id}
-                subtitle={pdata.title}
-                image={pdata.image}
-              />
-            ))
-          ))
-        }
-      </div>
-      {/* Video Testimonials */}
-      {
-          GVData.map((data) => (
-            data.basicdetails && data.basicdetails.map((pdata) => (
-              <VideoTestimonials
-                key={pdata.id}
-                color={pdata.color}
-              />
-            ))
-          ))}
-      {/* Our Projects */}
-      <h2 className={`text-${volunteerColor} flex justify-center text-3xl font-bold pb-14 p-8`}>Our Projects</h2>
-      <div className="flex flex-wrap justify-center">
-        {
-          GVData.map((info) => (
-            info.ourprojects && info.ourprojects.map((project) => (
-              <Projects
-                key={project.id}
-                title={project.title}
-                img={project.path}
-              />
-            ))
-          ))}
-        
-      </div>
-      {/* Our Destinations */}
-      <h2 className={`text-${volunteerColor} flex justify-center text-3xl font-bold pb-14 p-8`}>Our Destinations</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {
-          GVData.map((info) => (
-            info.ourdestination && info.ourdestination.map((destination) => (
-              <ProjectDestinations
-                key={destination.id}
-                countryname={destination.countryname}
-                projecttype={destination.projecttype}
-                path={destination.path}
-              />
-            ))
-          ))}
-      </div>
-      <br />
-      <Whyvolunteer />
-      <h2 className={`text-${volunteerColor} flex justify-center text-3xl font-bold pb-14 p-8`}>Process</h2>
-      <div className="flex flex-col justify-around md:flex-row">
-        {
-          GVData.map((info) => (
-            info.process && info.process.map((process) => (
-              <Process
-                key={process.id}
-                title={process.title}
-                image={process.image}
-                description={process.description}
-              />
-            ))
-          ))}
-      </div>
-      {/* Sign Up Banner */}
-      <div>
-      {
-          GVData.map((info) => (
-            info.signupbanner && info.signupbanner.map((banner) => (
-              <Signupbanner
-                key={banner.id}
-                content={banner.content}
-                img={banner.path}
-                url={banner.url}
-                
-              />
-            ))
-          ))}
-      </div>
-      <FnQ/>
-      <ContactFrom />
-      <Footer />
-      </div>
-    )}
-
-
-
-
-
-
-
-
-
-
-      
+        <SplashScreen finishLoading={() => setIsLoading(false)} />
+      ) : (
+        <div className=" overflow-hidden">
+          <Nav />
+          <HomeVideo />
+          <Counter />
+          {/* Product Intro */}
+          <div>
+            {GVData.map(
+              (data) =>
+                data.basicdetails &&
+                data.basicdetails.map((pdata) => (
+                  <Intro
+                    key={pdata.id}
+                    title={pdata.title}
+                    color={pdata.color}
+                    logo={pdata.logo}
+                    description={pdata.content}
+                    duration={pdata.duration}
+                    organization={pdata.organization}
+                    url={pdata.url}
+                  />
+                ))
+            )}
+          </div>
+          {/* Why Products */}
+          <div className="flex flex-col justify-around items-center md:flex-row pb-10">
+            {GVData.map(
+              (data) =>
+                data.whyproducts &&
+                data.whyproducts.map((pdata) => (
+                  <WhyProducts
+                    key={pdata.id}
+                    subtitle={pdata.title}
+                    image={pdata.image}
+                  />
+                ))
+            )}
+          </div>
+          {/* Video Testimonials */}
+          <div className="bg-global-volunteer">
+            {GVData.map(
+              (data) =>
+                data.basicdetails &&
+                data.basicdetails.map((pdata) => (
+                  <VideoTestimonials key={pdata.id} color={pdata.color} />
+                ))
+            )}
+          </div>
+          {/* Our Projects */}
+          <h2
+            className={`text-global-volunteer flex justify-center text-3xl font-bold pb-14 p-8`}
+          >
+            Our Projects
+          </h2>
+          <div className="flex flex-wrap justify-center">
+            {GVData.map(
+              (info) =>
+                info.ourprojects &&
+                info.ourprojects.map((project) => (
+                  <Projects
+                    key={project.id}
+                    title={project.title}
+                    img={project.path}
+                  />
+                ))
+            )}
+          </div>
+          {/* Our Destinations */}
+          <h2
+            className={`text-global-volunteer flex justify-center text-3xl font-bold pb-14 p-8`}
+          >
+            Our Destinations
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {GVData.map(
+              (info) =>
+                info.ourdestination &&
+                info.ourdestination.map((destination) => (
+                  <ProjectDestinations
+                    key={destination.id}
+                    countryname={destination.countryname}
+                    projecttype={destination.projecttype}
+                    path={destination.path}
+                  />
+                ))
+            )}
+          </div>
+          <br />
+          <Whyvolunteer />
+          <h2
+            className={`text-global-volunteer flex justify-center text-3xl font-bold pb-14 p-8`}
+          >
+            Process
+          </h2>
+          <div className="flex flex-col justify-around md:flex-row">
+            {GVData.map(
+              (info) =>
+                info.process &&
+                info.process.map((process) => (
+                  <Process
+                    key={process.id}
+                    title={process.title}
+                    image={process.image}
+                    description={process.description}
+                  />
+                ))
+            )}
+          </div>
+          {/* Sign Up Banner */}
+          <div>
+            {GVData.map(
+              (info) =>
+                info.signupbanner &&
+                info.signupbanner.map((banner) => (
+                  <Signupbanner
+                    key={banner.id}
+                    content={banner.content}
+                    img={banner.path}
+                    url={banner.url}
+                    color={banner.color}
+                  />
+                ))
+            )}
+          </div>
+          <FnQ />
+          <ContactFrom />
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
