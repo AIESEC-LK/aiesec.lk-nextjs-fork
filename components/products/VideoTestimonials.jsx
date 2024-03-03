c1476272a8f54825ec87440b57647334d8d8b369
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-03-04 01:02:52 +0530

    updating destinations

--- components/products/VideoTestimonials.jsx
+++ components/products/VideoTestimonials.jsx
@@ -6,7 +6,7 @@ function VideoTestimonials(props) {
   return (
     <div className={`pt-10 pb-5`}>
       <h2 className=" text-center text-3xl font-semibold pb-4 px-3 text-white">
-        What they’re talking about Volunteering with AIESEC
+        What they’re talking about AIESEC
       </h2>
       <div>
       <Video />
