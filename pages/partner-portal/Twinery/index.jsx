a669a2b905314d6882f0bc516cef74abcec0102d
Ashan Dimantha <ashandimanthalk@gmail.com>
2024-03-20 00:55:16 +0530

    initiate: partner-portal-video

--- pages/partner-portal/Twinery/index.jsx
+++ pages/partner-portal/Twinery/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Nav from '../../../components/Nav'
+import HtmlHead from '../../../components/partner-portal/HtmlHead'
+import Footer from '@components/Footer'
+import TwineryHomeVideo from '@components/partner-portal/Twinery/TwineryHomeVideo'
 
 function index() {
   return (
-    <div>index</div>
+    <div>
+      <HtmlHead title={"Twinery Partner Portal - AIESEC in Sri Lanka"} description={"Explore Twinery partners and opportunities."}/>
+      <Nav/>
+      <TwineryHomeVideo/>
+      <br />
+      <Footer/>
+    </div>
   )
 }
 
